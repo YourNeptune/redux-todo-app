1d55caa19adbfb6b23bbb687c61286ebda595ad6
Lixuan Luo <lixuanluo@hotmail.com>
2021-05-31 21:01:16 -0400

    input

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import React from "react";
 import "./App.css";
+import Input from './components/Input'
 
 function App() {
-  return <div className="App"></div>;
+  return <div className="App">
+  {/* todo list */}
+    {/* done */}
+  {/* input */}
+  <Input/>
+  </div>;
 }
 
 export default App;
